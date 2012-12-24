4c29a9b8a8d1b5d2d3816b24d0522e17f9b0a693
richo <richo@psych0tik.net>
2012-12-24 21:09:44 +1100

    Remove unused gizmo class

--- groundstation/gizmo.py
+++
@@ -1,8 +0,0 @@
-import pygit2
-
-class Gizmo(pygit2.Object):
-    """Storage primitive from which all other primitives are derived.
-
-    Present implementation backs onto git's object database."""
-
-    pass
