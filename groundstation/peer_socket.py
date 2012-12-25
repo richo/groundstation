b28630096d071ebf0e4789d1bbc2ff9abc3a2829
richo <richo@psych0tik.net>
2012-12-25 16:07:07 +1100

    Encapsulate peer connections.
    groundstation now implements an echo server

---
+++ groundstation/peer_socket.py
@@ -0,0 +1,48 @@
+import groundstation.logger
+log = groundstation.logger.getLogger(__name__)
+
+class PeerSocket(object):
+    """Wrapper for a peer who just connected, or one we've connected to
+
+    Since the communication protocol should be implicitly bidirectional, the
+    factory methods should be the only instanciation methods"""
+
+    def __init__(self, conn, peer):
+        self.conn = conn
+        self.peer = peer
+        self.queue = []
+
+    @classmethod
+    def from_accept(klass, args):
+        return klass(*args)
+
+    @classmethod
+    def from_connect(klass, args):
+        return klass(*args)
+
+    def fileno(self):
+        return self.conn.fileno()
+
+    def __repr__(self):
+        return "<%s: from %s>" % (self.__class__, self.peer)
+
+    def has_data_ready(self):
+        return len(self.queue) > 0
+
+    def enqueue(self, data):
+        self.queue = [data] + self.queue
+
+    def recv(self):
+        """Recieve some bytes fromt he socket, handling buffering internally"""
+        data = self.conn.recv(1024)
+        log.debug("RECV %i bytes: %s from %s" %
+                (len(data), repr(data), self.peer))
+        return data # TODO Buffering
+
+    def send(self):
+        """Send some data that's presently in the queue"""
+        assert self.has_data_ready(), "Attempt to send without data ready"
+        data = self.queue.pop()
+        log.debug("SEND %i bytes: %s to %s" %
+                (len(data), repr(data), self.peer))
+        self.conn.send(data)
--- groundstation/sockets/stream_socket.py
+++ groundstation/sockets/stream_socket.py
@@ -1,11 +1,18 @@
 import socket
 
+import groundstation.logger
+log = groundstation.logger.getLogger(__name__)
+
+from groundstation.peer_socket import PeerSocket
+
 class StreamSocket(object):
     """Wraps a TCP socket"""
     def __init__(self, port):
         self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._sock.bind(('0.0.0.0', port))
         self._sock.listen(16)
+        # XXX Implement the queue as a seperate class/
+        self.write_queue  = []
 
     def fileno(self):
         """Return the underlying socket to make select() work"""
@@ -16,4 +23,14 @@ class StreamSocket(object):
         return self._sock
 
     def accept(self):
-        return self._sock.accept()
+        p = self._sock.accept()
+        log.info("Accepted a connection from %s" % repr(p[1]))
+        return PeerSocket.from_accept(p)
+
+    def enqueue(self, data):
+        """Enqueues data for writing inside the select loop"""
+        self.write_queue.append(data)
+
+    def data_ready(self):
+        """(bool) does this socket have enqueued data ready"""
+        return len(self.write_queue) > 0
