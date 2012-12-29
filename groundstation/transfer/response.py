9872587d8a9e5cb51b77af1f52b078b0f5b31740
richo <richo@psych0tik.net>
2012-12-29 22:17:21 +1100

    Support for serializing responses

--- groundstation/transfer/response.py
+++ groundstation/transfer/response.py
@@ -1,9 +1,20 @@
+from groundstation.proto.gizmo_pb2 import Gizmo
+
 class Response(object):
     def __init__(self, response_to, payload):
         self.type = "RESPONSE"
         self.id = response_to
         if payload is not None:
-            self.phase = "TRANSFER"
+            self.verb = "TRANSFER"
         else:
-            self.phase = "TERMINATE"
+            self.verb = "TERMINATE"
         self.payload = payload
+
+    def SerializeToString(self):
+        gizmo = Gizmo()
+        gizmo.id = str(self.id)
+        gizmo.type = Gizmo.REQUEST
+        gizmo.verb = self.verb
+        if self.payload:
+            gizmo.payload = self.payload
+        return gizmo.SerializeToString()
