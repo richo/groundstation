69d8e9d382d208f2d9354dd08286afa842d0dc5b
richo <richo@psych0tik.net>
2012-12-29 19:50:25 +1100

    Deal with invalid Gizmo's on the wire

--- TODO.md
+++ TODO.md
@@ -1,3 +1,4 @@
 * Rip out the ad hoc payload transfer stuff, use protocol buffers
 * Boilerplate to allow enqueue()'ing an iterator to avoid being a noisy neighbour.
 * Rework the request/reply cycle and the mechanism to pass a Station into the request/response factories
+* Unify on FooException or FooError
--- groundstation/gizmo_factory.py
+++ groundstation/gizmo_factory.py
@@ -1,6 +1,11 @@
 from proto.gizmo_pb2 import Gizmo
+from google.protobuf.message import DecodeError
 from transfer.request import Request
 from transfer.response import Response
+
+import groundstation.logger
+log = groundstation.logger.getLogger(__name__)
+
 class GizmoFactory(object):
     builders = {
             Gizmo.REQUEST: Request,
@@ -10,8 +15,17 @@ class GizmoFactory(object):
         self.station = station
 
     def hydrate(self, data, stream):
-        gizmo = Gizmo.ParseFromString(data)
+        gizmo = Gizmo()
+        try:
+            gizmo.ParseFromString(data)
+        except DecodeError:
+            raise InvalidGizmoError("Couldn't decode gizmo")
         try:
             self.builders[gizmo.type](gizmo, self.station, stream)
         except KeyError:
             raise Exception("Invalid message type")
+
+class InvalidGizmoError(Exception):
+    def __init__(self, msg):
+        log.warn(msg)
+        super(InvalidGizmoError, self).__init__(self)
