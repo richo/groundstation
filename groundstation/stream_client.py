d021406cc86d79b63bf103f374091b6b964692e7
richo <richo@psych0tik.net>
2012-12-29 20:50:33 +1100

    Wire up serialization to protobuf

--- groundstation/stream_client.py
+++ groundstation/stream_client.py
@@ -1,7 +1,11 @@
 import socket
 from sockets.stream_socket import StreamSocket
 from sockets.socket_closed_exception import SocketClosedException
+from transfer.request import Request
 import settings
+
+import groundstation.logger
+log = groundstation.logger.getLogger(__name__)
 
 class StreamClient(StreamSocket):
     def __init__(self, addr):
@@ -10,10 +14,20 @@ class StreamClient(StreamSocket):
         self._sock.connect((addr, settings.PORT))
 
     def begin_handshake(self):
-        self.enqueue("LISTALLOBJECTS")
+        request = Request("LISTALLOBJECTS")
+        self.enqueue(request)
+
 
     def recv(self):
-        return self.socket.recv(settings.DEFAULT_BUFSIZE)
+        """Recieve some bytes fromt he socket, handling buffering internally"""
+        # TODO Duped with PeerSocket
+        data = self.socket.recv(settings.DEFAULT_BUFSIZE)
+        if not data:
+            self.socket.close()
+            raise StreamSocketClosedException(self)
+        log.debug("RECV %i bytes: %s from %s" %
+                (len(data), repr(data), self.peer))
+        return data # TODO Buffering
 
 class StreamSocketClosedException(SocketClosedException):
     """Raised when a peer closes their socket"""
--- groundstation/transfer/request.py
+++ groundstation/transfer/request.py
@@ -1,20 +1,51 @@
 import uuid
 
-VALID_REQUESTS = [
-        "LISTALLBJECTS",
-        "FETCHOBJECT"
-]
+from groundstation.proto.gizmo_pb2 import Gizmo
 
 class InvalidRequest(Exception):
     pass
 
 class Request(object):
-    def __init__(self, request):
+    VALID_REQUESTS = {
+            "LISTALLOBJECTS": "handle_listallobjects",
+            "FETCHOBJECT": "handle_fetchobject"
+    }
+
+    def __init__(self, verb, station=None, stream=None, payload=None):
         self.type = "REQUEST"
         self.id = uuid.uuid1()
-        self.request = request
+        self.verb = verb
+        self.station = station
+        self.stream = stream
+        self.payload = payload
         self.validate()
 
+    # @property
+    # def station(self):
+    #     return self.station
+
+    # @station.setter
+    # (self, station):
+    #     self.station = station
+
+    def SerializeToString(self):
+        gizmo = Gizmo()
+        gizmo.id = str(self.id)
+        gizmo.type = Gizmo.REQUEST
+        gizmo.verb = self.verb
+        if self.payload:
+            gizmo.payload = self.payload
+        return gizmo.SerializeToString()
+
     def validate(self):
-        if self.request not in VALID_REQUESTS:
-            raise Exception("Invalid Request: %s" % (self.request))
+        if self.verb not in self.VALID_REQUESTS:
+            raise Exception("Invalid Request: %s" % (self.verb))
+
+    def handle(self):
+        self.__getattribute__(self.VALID_REQUESTS[self.verb])()
+
+    def handle_listallobjects(self):
+        pass # TODO
+
+    def handle_fetchobject(self):
+        pass # TODO
