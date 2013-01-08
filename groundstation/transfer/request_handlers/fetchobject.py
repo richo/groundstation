88fdabfe5427ec68c036783b509a520b6a197255
richo <richo@psych0tik.net>
2013-01-08 23:32:34 +1100

    Send serialized git objects

--- groundstation/transfer/request_handlers/fetchobject.py
+++ groundstation/transfer/request_handlers/fetchobject.py
@@ -1,9 +1,17 @@
 import groundstation.transfer.request
 
+from groundstation.proto.git_object_pb2 import GitObject
+
 from groundstation import logger
 log = logger.getLogger(__name__)
 
 def handle_fetchobject(self):
     log.info("Handling FETCHOBJECT for %s" % (repr(self.payload)))
-    response = self._Response(self.id, "TRANSFER", self.station.repo[self.payload])
+    git_obj = self.station.repo[self.payload]
+    git_pb = GitObject()
+
+    git_pb.type = git_obj.type
+    git_pb.data = git_obj.data
+
+    response = self._Response(self.id, "TRANSFER", git_pb.SerializeToString())
     self.stream.enqueue(response)
--- groundstation/transfer/response_handlers/transfer.py
+++ groundstation/transfer/response_handlers/transfer.py
@@ -1,8 +1,12 @@
+from groundstation.proto.git_object_pb2 import GitObject
+
 from groundstation import logger
 log = logger.getLogger(__name__)
 
 
 def handle_transfer(self):
-    log.info("Handling TRANSFER of %s" % (self.payload))
-    ret = self.station.write_object(self.payload)
+    git_pb = GitObject()
+    git_pb.ParseFromString(self.payload)
+    log.info("Handling TRANSFER of %s" % (git_pb.type))
+    ret = self.station.repo.write(git_pb.type, git_pb.data)
     log.info("Wrote object %s" % (repr(ret)))
