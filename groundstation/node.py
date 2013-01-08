4d27278d5a028302d0fa4a776f38fea6a0e0480e
richo <richo@psych0tik.net>
2013-01-08 21:27:50 +1100

    Log node creation

--- groundstation/node.py
+++ groundstation/node.py
@@ -1,6 +1,10 @@
 import uuid
 
+from groundstation import logger
+log = logger.getLogger(__name__)
+
 class Node(object):
     def __init__(self):
         self.uuid = uuid.uuid1()
         self.name = str(self.uuid)
+        log.info("Node started with id %s" % self.name)
