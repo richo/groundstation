d6d0d2c5c8af6acc7ba4cf6fb7a3f691f97b3f98
richo <richo@psych0tik.net>
2013-01-09 22:13:02 +1100

    Hose unused reimplementations

--- groundstation/transfer/v2/request.py
+++
@@ -1,8 +0,0 @@
-class GeneratedResponse(object):
-    """A response object that would be generated in response to a hydrated Request"""
-
-
-class Request(object):
-    """A Request object that can be serialized and enqueued to be sent"""
-    def __init__(self, verb, station=None, stream=None, payload=None, origin=None):
-
--- groundstation/transfer/v2/response.py
+++
@@ -1,4 +0,0 @@
-from groundstation.proto.gizmo_pb2 import Gizmo
-
-from groundstation import logger
-log = logger.getLogger(__name__)
