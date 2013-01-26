08f0da7d05c580cc6d92470f94664ff93a6c02a8
richo <richo@psych0tik.net>
2013-01-26 21:24:32 +1100

    Final tests working

---
+++ test/store_fixture.py
@@ -0,0 +1,22 @@
+import unittest
+
+import shutil
+
+
+def random_path():
+    # TODO
+    return "/tmp/groundstation"
+
+
+class StoreTestCase(unittest.TestCase):
+    storeClass = None
+
+    def setUp(self):
+        self.path = random_path()
+        self.repo = self.storeClass(self.path)
+
+    def tearDown(self):
+        shutil.rmtree(self.path)
+
+    def test_create_blob(self):
+        blob = self.repo.create_blob("rawr lol butts")
