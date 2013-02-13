8bc9b1b9a0f9816944939d02c30c51a8ef21239a
richo <richo@psych0tik.net>
2013-02-13 23:16:01 +1100

    Basucally dupe RootObject into UpdateObject

--- groundstation/objects/update_object.py
+++ groundstation/objects/update_object.py
@@ -1,5 +1,29 @@
 import update_object_pb2
 
+class UpdateObject(object):
+    data_members = [parents, data]
+    def __init__(self, parents, data):
+        self.parents = parents
+        self.data = data
+        self.sha1 = None
 
-class UpdateObject(update_object_pb2.UpdateObject):
-    pass
+    @property
+    def sha1(self):
+        if self.sha1:
+            return self.sha1
+        # XXX Calculate sha1 of this object
+
+    @staticmethod
+    def from_object(obj):
+        """Convert an object straight from Git into a RootObject"""
+        protobuf = update_object_pb2.UpdateObject()
+        protobuf.ParseFromString(obj)
+        return UpdateObject(protobuf.parents, protobuf.data)
+
+    def as_object(self):
+        """Convert a RootObject into data suitable to write into the database
+        as a Git object"""
+        protobuf = update_object_pb2.UpdateObject()
+        for member in self.data_members:
+            setattr(protobuf, member, getattr(self, member))
+        return protobuf.SerializeToString()
