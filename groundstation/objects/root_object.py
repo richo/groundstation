6db0279d74fda1ee4df541aeff21c1712402d191
richo <richo@psych0tik.net>
2013-02-13 20:58:05 +1100

    Create stub objects for internal consumption

---
+++ groundstation/objects/root_object.py
@@ -0,0 +1,5 @@
+import root_object_pb2
+
+
+class RootObject(root_object_pb2.RootObject):
+    pass
---
+++ groundstation/objects/update_object.py
@@ -0,0 +1,5 @@
+import update_object_pb2
+
+
+class UpdateObject(update_object_pb2.UpdateObject):
+    pass
