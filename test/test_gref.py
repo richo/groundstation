ec54e6d40de350db3e92c07a49471fe5efcf6ff4
richo <richo@psych0tik.net>
2013-02-15 03:24:11 +1100

    Excercise remove_tip

--- test/test_gref.py
+++ test/test_gref.py
@@ -11,3 +11,16 @@ class TestGitGref(store_fixture.StoreTestCase):
         gref = Gref(self.repo, "testchannel", "test_write_tip")
         gref.write_tip("foobarbaz", "")
         self.assertEqual(list(gref), ["foobarbaz"])
+
+    def test_remove_tip(self):
+        gref = Gref(self.repo, "testchannel", "test_write_tip")
+        gref.write_tip("foobarbaz", "")
+        gref.write_tip("lulzbutts", "")
+        gref.write_tip("buttslols", "")
+
+        gref.remove_tip("lulzbutts")
+
+        self.assertTrue("foobarbaz" in list(gref))
+        self.assertTrue("buttslols" in list(gref))
+        self.assertFalse("lulzbutts" in list(gref))
+
