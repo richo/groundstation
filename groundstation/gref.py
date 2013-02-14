48d23530841fe7ac08cf3bfc6263fe4f95a40d27
richo <richo@psych0tik.net>
2013-02-15 04:57:43 +1100

    Implement __str__ on Gref

--- groundstation/gref.py
+++ groundstation/gref.py
@@ -9,6 +9,8 @@ class Gref(object):
         self._node_path = os.path.join(self.store.gref_path(),
                                  self.channel,
                                  self.identifier)
+    def __str__(self):
+        return "%s/%s" % (self.channel, self.identifier)
 
     def exists(self):
         return os.path.exists(self._node_path)
