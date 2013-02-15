741eeafc220d4a5b0bcbfe855aed51cfabb1b992
richo <richo@psych0tik.net>
2013-02-15 21:06:30 +1100

    Wire up sync of channel objects

--- airship/__init__.py
+++ airship/__init__.py
@@ -1,7 +1,15 @@
 import os
 import json
 
-from flask import Flask
+from flask import Flask, render_template
+
+
+def channels_json(station, escaped=False):
+    channels = [{"name": channel} for channel in station.channels()]
+    jsonbody = json.dumps(channels)
+    if escaped:
+        jsonbody = jsonbody.replace("</", "<\\/")
+    return jsonbody
 
 
 def make_airship(station):
@@ -9,11 +17,10 @@ def make_airship(station):
 
     @app.route("/")
     def index():
-        return render_template("index.html",
-                channels=station.channels())
+        return render_template("index.html", channels_json=channels_json(station, True))
 
     @app.route("/channels")
     def list_channels():
-        return json.dumps(station.channels())
+        return channels_json(station)
 
     return app
