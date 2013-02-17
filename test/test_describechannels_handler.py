467f5d6aab6757b27f3af0f56a44bfee71cb57c0
richo <richo@psych0tik.net>
2013-02-17 14:58:42 +1000

    Implement half a test for refs to missing objects

--- test/test_describechannels_handler.py
+++ test/test_describechannels_handler.py
@@ -12,6 +12,8 @@ import pygit2
 from groundstation import logger
 log = logger.getLogger(__name__)
 
+import binascii
+
 def _payload(obj, gref, tips):
     chunk = ChannelList()
     description = chunk.channels.add()
@@ -47,12 +49,30 @@ class TestHandlerDescribeChannels(StationHandlerTestCase):
         handle_describechannels(self.station)
         self.assertEqual([current_oid], gref.tips())
         current_oid = root_oid
-        for i in xrange(10):
-            update_obj = UpdateObject([current_oid], "loldata")
-            current_oid = self.station.station.write(root_obj.as_object())
-        self.station.station.update_gref(gref, [root_oid])
-        self.assertEqual([current_oid], gref.tips())
 
-        self.station.station.update_gref(gref, [root_oid], [])
+    # XXX Disabled for now, successully throws keyerrors when writing out invalid refs though
+    def handle_describechannels_for_missing_tip(self):
+        test_id = u"tests/1"
+        test_channel = u"test_channel"
+        test_protocol = u"test_protocol"
+
+        gref = Gref(self.station.station.store, test_channel, test_id)
+
+        root_obj = RootObject(test_id, test_channel, test_protocol)
+        root_oid = self.station.station.write(root_obj.as_object())
+        current_oid = [root_oid]
+
+        for i in xrange(10):
+            update_obj = UpdateObject([current_oid.pop()], "loldata")
+            current_oid.append(self.station.station.write(update_obj.as_object()))
+
+        update_obj = UpdateObject([current_oid.pop()], "loldata")
+        current_oid.append(binascii.hexlify(pygit2.hash(update_obj.as_object())))
+
+        self.station.station.update_gref(gref, [root_oid])
+        self.assertEqual([root_oid], gref.tips())
+        current_oid = current_oid.pop()
+
         self.station.payload = _payload(update_obj, gref, [current_oid])
         handle_describechannels(self.station)
+        self.assertEqual([current_oid], gref.tips())
