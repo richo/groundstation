0f68af0b8cf32b6d21bdc7f0e5888d953268686a
richo <richo@psych0tik.net>
2013-02-17 11:32:56 +1000

    Update listallchannels to match new proto spec

--- groundstation/transfer/request_handlers/listallchannels.py
+++ groundstation/transfer/request_handlers/listallchannels.py
@@ -9,8 +9,15 @@ def handle_listallchannels(self):
     payload = self.station.channels()
     log.info("Sending %i object descriptions" % (len(payload)))
     chunk = groundstation.proto.channel_list_pb2.ChannelList()
-    for obj in payload:
-        chunk.channelname.append(payload)
+    for channel in payload:
+        description = chunk.channels.add()
+        grefs = self.station.grefs(channel)
+        for gref in grefs:
+            _gref = description.grefs.add()
+            _gref.name = gref.identifier
+            for tip in gref:
+                _tip = _gref.tips.add()
+                _tip.tip = tip
     response = self._Response(self.id, "DESCRIBECHANNELS",
             chunk.SerializeToString())
     self.stream.enqueue(response)
