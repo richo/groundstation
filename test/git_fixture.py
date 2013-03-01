c929f8741adc1ca1bfed3b002e911b60fe781756
richo <richo@psych0tik.net>
2013-03-02 10:52:50 +1100

    Add tree fixture

---
+++ test/git_fixture.py
@@ -0,0 +1,6 @@
+def fake_tree():
+    return """100644 blob fadc864ddfed4a93fabf6d23939db4d542eb4363
+.gitignore100644 blob 48e87b133a2594371acd57c49339dc8c04d55146    .gitmodules
+100644 blob 725455bca81c809ad55aac363c633988f9207620    .jshintignore
+100644 blob 40928639c7903f83f26e1aed78401ffde587e437    .jshintrc
+100644 blob f3a9c9a807be340a7b929557aea3088540c77a6c    .rbenv-version"""
