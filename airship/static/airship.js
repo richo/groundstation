72c4e82533490c2865babee476ae4bd8423fe4c6
richo <richo@psych0tik.net>
2013-03-19 21:36:59 +1100

    Refactor arrows to alternate sides

--- airship/static/airship.js
+++ airship/static/airship.js
@@ -47,13 +47,27 @@ var Grefs = Backbone.Collection.extend({
   model: Gref
 });
 
-var rightWardAnchor = {
-				connector:"StateMachine",
-				paintStyle:{lineWidth:3,strokeStyle:"#056"},
-				endpoint:"Blank",
-				anchor:[[1, 0, 1, 0], [1, 1, 1, 0]],
-				overlays:[ ["PlainArrow", {location:1, width:7, length:3} ]]
-};
+var get_anchor = (function (){
+    var anchors = {
+        0: {
+            connector:"StateMachine",
+            paintStyle:{lineWidth:3,strokeStyle:"#056"},
+            endpoint:"Blank",
+            anchor:[[1, 0, 1, 0], [1, 1, 1, 0]],
+            overlays:[ ["PlainArrow", {location:1, width:7, length:3} ]]
+            },
+        1: {
+            connector:"StateMachine",
+            paintStyle:{lineWidth:3,strokeStyle:"#056"},
+            endpoint:"Blank",
+            anchor:[[0, 0, 1, 0], [0, 1, 1, 0]],
+            overlays:[ ["PlainArrow", {location:1, width:7, length:3} ]]
+            }
+    };
+    return function(side) {
+        return anchors[side % 2];
+    };
+})();
 
 var GrefMenuItem = Backbone.View.extend({
   tagName: "li",
@@ -221,14 +235,23 @@ var RenderedGref = Backbone.View.extend({
       }
     });
 
+    var links = {};
+    var side = 0;
     _.each(content, function(item) {
       _.each(item.parents, function(parent_id) {
+        if (links[parent_id] === undefined)
+          links[parent_id] = [parent_id];
+        else {
+          links[parent_id].push(parent_id);
+          side++;
+        }
+
         if ($("#" + parent_id).length > 0) {
           console.log("Creating links");
           item.connections.push(jsPlumb.connect({
             source:item.hash,
             target:parent_id
-          }, rightWardAnchor));
+          }, get_anchor(side)));
         } else {
           console.log("Not creating links, pretty sure it's a root node");
         }
