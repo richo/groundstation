98d87822523895403dcf6918f6fc3bfc49464078
richo <richo@psych0tik.net>
2013-03-23 16:19:05 +1100

    Refactor validation logic to be more testable

--- airship/static/airship-gref-validation.js
+++ airship/static/airship-gref-validation.js
@@ -1,5 +1,21 @@
-groundstation.validators.gref = function(name, title, body) {
-  if (name.indexOf(" ") >= 0)
-    return false;
-  return true;
-};
+groundstation.validators.gref = (function() {
+  func = function(name, title, body) {
+    return func.valid_name_p(name) &&
+           func.valid_title_p(title) &&
+           func.valid_body_p(body);
+  };
+  func.valid_name_p = function(name) {
+    if (name.indexOf(" ") >= 0)
+      return false;
+    return true;
+  };
+  func.valid_title_p = function(title) {
+    // TODO
+    return true;
+  };
+  func.valid_body_p = function(body) {
+    // TODO
+    return true;
+  };
+  return func;
+})();
--- airship/test/test_gref_validator.js
+++ airship/test/test_gref_validator.js
@@ -9,6 +9,9 @@ eval(fs.readFileSync("static/airship-gref-validation.js").toString());
 
 describe('groundstation.validators.gref', function(){
   it('should return false if spaces in name', function(){
-    assert.equal(false, groundstation.validators.gref("butts lol", "oktitle", "okbody"));
+    assert.equal(false, groundstation.validators.gref.valid_name_p("butts lol"));
+  });
+  it('should return true for short valid names', function(){
+    assert.equal(true, groundstation.validators.gref.valid_name_p("buttslol"));
   });
 });
