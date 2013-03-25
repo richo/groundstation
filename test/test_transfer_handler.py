3db48de39626d90d6f0f7a635056293d3adb638b
richo <richo@psych0tik.net>
2013-03-25 16:50:42 +1100

    Assert that we reject invalid transfers

--- test/test_transfer_handler.py
+++ test/test_transfer_handler.py
@@ -26,3 +26,20 @@ class TestHandlerTransfer(StationHandlerTestCase):
         handle_transfer(self.station)
 
         self.assertTrue(self.station.station[oid2hex(oid)], object_body)
+
+    def test_rejects_invalid_transfers(self):
+        self.station.set_real_id(True)
+
+        object_body = "foo bar baz butts lol"
+        oid = pygit2.hash(object_body)
+
+        req = MockRequest(self.station.id)
+        req.payload = "deadbeefdeadbeefdeadbeefdeadbeefdeadbeef"
+
+        self.station.station.registry.register(req)
+
+        git_pb = GitObject()
+        git_pb.data = object_body
+        git_pb.type = pygit2.GIT_OBJ_BLOB
+        self.station.payload = git_pb.SerializeToString()
+        self.assertRaises(AssertionError, handle_transfer, self.station)
