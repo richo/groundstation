0e6e9fbcb4323ae6858cc68944297e3c67cd2fc8
richo <richo@psych0tik.net>
2013-03-30 21:41:16 +1100

    Test deals with untrusted signatures

--- test/test_gref_marshall.py
+++ test/test_gref_marshall.py
@@ -63,3 +63,20 @@ class TestGrefMarshall(store_fixture.StoreTestCase):
         marshalled = gref.marshall(crypto_adaptor=adaptor)
 
         self.assertEqual(marshalled["signatures"][update_oid], "valid")
+
+    def test_marshalls_untrusted_signatures(self):
+        gref = Gref(self.repo, "testchannel", "test_untrusted_signature")
+        adaptor = RSAAdaptor({
+            "valid": crypto_fixture.passphrase_pubkey
+            })
+        private_adaptor = RSAPrivateAdaptor(crypto_fixture.valid_key)
+
+        root = root_object("test", "test_channel", "test")
+        root_oid = self.repo.create_blob(root.as_object())
+        update = update_object("test object", [root_oid])
+        update_oid = self.repo.create_blob(update.as_object())
+        gref.write_tip(update_oid, private_adaptor.sign(update_oid))
+
+        marshalled = gref.marshall(crypto_adaptor=adaptor)
+
+        self.assertFalse(marshalled["signatures"][update_oid])
