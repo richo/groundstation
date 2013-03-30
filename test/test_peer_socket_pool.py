5c9b724c9696e94c1f44de955c0c969f91ec415b
richo <richo@psych0tik.net>
2013-03-30 17:28:57 +1100

    Start testing peer_socket_pool

---
+++ test/test_peer_socket_pool.py
@@ -0,0 +1,21 @@
+import unittest
+
+from peer_socket_pool_fixture import MockPeer
+
+from groundstation.peer_socket_pool import PeerSocketPool
+
+class TestPeerSocketPool(unittest.TestCase):
+    def test_add_remove(self):
+        pool = PeerSocketPool()
+        peer = MockPeer('127.0.0.1')
+
+        pool.append(peer)
+        pool.remove(peer)
+
+        self.assertEqual(0, len(pool))
+
+    def test_throws_error_on_remove_nonexistant_peer(self):
+        pool = PeerSocketPool()
+        peer = MockPeer('127.0.0.1')
+
+        self.assertRaises(AttributeError, pool.remove, peer)
