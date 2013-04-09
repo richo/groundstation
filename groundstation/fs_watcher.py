a677b5dec06fe9f6a884d1c37a9d2b34f762ec3e
richo <richo@psych0tik.net>
2013-04-09 22:54:50 +1000

    I'll write my own FS wrapper. With blackjack. And hookers.
    And also syncronous operations, like the FSM intended.

--- groundstation/fs_watcher.py
+++ groundstation/fs_watcher.py
@@ -1,38 +1,68 @@
+import os
+import signal
+
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler, FileCreatedEvent
 
-
 class FSHandler(FileSystemEventHandler):
-    def __init__(self, watcher):
-        self.watcher = watcher
+    def __init__(self, pipe):
+        self.pipe = pipe
         super(FSHandler, self).__init__()
 
     def on_created(self, event):
         if isinstance(event, FileCreatedEvent):
-            for handler in self.watcher.handlers:
-                handler(event)
+            sent = 0
+            data = event.src_path + chr(0)
+            data_len = len(data)
+            while sent != data_len:
+                sent += os.write(self.pipe, data[sent:])
 
 
-class FSWatcher(object):
-    """Watcher for filesystems
+class Watcher(object):
+    """Wrapper object to allow for being passed into select()"""
+    def __init__(self, pid, pipe):
+        self.pid = pid
+        self.pipe = pipe
 
-    Discards all directory events, since files are the only things we care
-    about
-    """
+    def fileno(self):
+        return self.pipe
 
-    def __init__(self, path, defer=False):
-        self.path = path
-        self.observer = Observer()
-        self._handler = FSHandler(self)
+    def read(self):
+        """Block until we can read a full, comma seperated path out"""
+        nul = chr(0)
+        buf = ""
+        while True:
+            c = os.read(self.pipe, 1)
+            if c == nul:
+                break
+            buf += c
+        assert len(buf) > 0, "Didn't get any data from pipe"
+        return buf
 
-        self.observer.schedule(self._handler, path=self.path, recursive=True)
-        self.handlers = []
+    def kill(self):
+        # Use sigkill because we can't risk the race conditions.
+        os.kill(self.pid, signal.SIGKILL)
 
-        if not defer:
-            self.start()
 
-    def start(self):
-        self.observer.start()
 
-    def register_handler(self, handler):
-        self.handlers.append(handler)
+def FSWatcher(path):
+    _out, _in = os.pipe()
+    _pid = os.fork()
+    if _pid > 0:
+        return Watcher(_pid, _out)
+    elif _pid == 0:
+        # Run doesn't create a new thread
+        watch(path, _in)
+    else:
+        raise Exception("fork()")
+
+
+def watch(path, pipe):
+    path = path
+    pipe = pipe
+    observer = Observer()
+    handler = FSHandler(pipe)
+    observer.schedule(handler, path=path, recursive=True)
+    # Write a null to the pipe so we know we're ready
+    os.write(pipe, chr(0))
+    observer.run()
--- test/test_station_object_watcher.py
+++ test/test_station_object_watcher.py
@@ -1,27 +1,40 @@
-import time
+import os
+import select
 
 from station_fixture import StationTestCase
 from groundstation.fs_watcher import FSWatcher
+from groundstation.utils import path2id
 
 
 class TestFSNotifcation(StationTestCase):
     def test_fs_notification(self):
-        ret = []
         watcher = FSWatcher(self.station.store.object_root)
+        self.assertEqual(chr(0), os.read(watcher.pipe, 1))
 
-        @watcher.register_handler
-        def handler(event):
-            ret.append(event)
+        _id = self.station.write("foobarbaz")
+        ret = watcher.read()
+        self.assertEqual(_id, path2id(ret))
 
-        self.station.write("foobarbaz")
+        _id = self.station.write("foobarbaz1")
+        ret = watcher.read()
+        self.assertEqual(_id, path2id(ret))
 
-        self.station.write("foobarbaz1")
-
+        _id = self.station.write("foobarbaz2")
         self.station.write("foobarbaz2")
-        self.station.write("foobarbaz2")
-        time.sleep(.1)
+        ret = watcher.read()
+        self.assertEqual(_id, path2id(ret))
 
-        self.assertEqual(len(ret), 3)
+        watcher.kill()
 
-        watcher.observer.stop()
-        watcher.observer.join()
+    def test_selects_sanely(self):
+        watcher = FSWatcher(self.station.store.object_root)
+        self.assertEqual(chr(0), os.read(watcher.pipe, 1))
+
+        (s_read, s_write, s_exc) = select.select([watcher.pipe], [], [], 0)
+        self.assertEqual(len(s_read), 0)
+
+        self.station.write("newdata")
+        (s_read, s_write, s_exc) = select.select([watcher.pipe], [], [], 1)
+        self.assertEqual(len(s_read), 1)
+
+        watcher.kill()
