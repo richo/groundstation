fb531e21429c143e67e7e16dac9b3eafd292f13a
Richo Healey <richo@psych0tik.net>
2013-04-21 05:12:38 +1000

    Test that we can send objects between nodes

--- test/test_station_integration.py
+++ test/test_station_integration.py
@@ -1,12 +1,18 @@
 import os
 import socket
+import select
 
 import unittest
 import tempfile
 import shutil
 
+from groundstation.node import Node
+from groundstation.station import Station
+
 from groundstation.stream_listener import StreamListener
 from groundstation.stream_client import StreamClient
+
+from groundstation.peer_socket import PeerSocket
 
 
 class TestListener(StreamListener):
@@ -29,6 +35,8 @@ class TestClient(StreamClient):
 class StationIntegrationFixture(unittest.TestCase):
     def setUp(self):
         self.dir = tempfile.mkdtemp()
+        self.node = Node()
+        self.station = Station(os.path.join(self.dir, "station"), self.node)
 
     def tearDown(self):
         shutil.rmtree(self.dir)
@@ -39,3 +47,33 @@ class StationConnectionTestCase(StationIntegrationFixture):
         addr = os.path.join(self.dir, "listener")
         listener = TestListener(addr)
         client = TestClient(addr)
+
+
+class StationCommunication(StationIntegrationFixture):
+    def test_send_objects(self):
+        read_sockets = [];
+        write_sockets = [];
+        def tick():
+            return select.select(read_sockets, write_sockets, [], 1)
+
+        addr = os.path.join(self.dir, "listener")
+        listener = TestListener(addr)
+        read_sockets.append(listener)
+
+        client = TestClient(addr)
+        write_sockets.append(client)
+
+        (sread, swrite, _) = tick()
+        # Handle our listener
+        self.assertEqual(len(sread), 1)
+        peer = listener.accept(PeerSocket)
+        read_sockets.append(peer)
+        write_sockets.append(peer)
+
+        self.assertEqual(len(swrite), 1)
+
+        client.begin_handshake(self.station)
+        client.send()
+
+        (sread, swrite, _) = tick()
+        self.assertEqual(sread, [peer])
