04ea8c37b8419dc504c5f4a9e19d0c2c09f2094d
Richo Healey <richo@psych0tik.net>
2013-05-30 13:19:43 +1000

    Testcases for new type logic

--- groundstation/objects/object_factory.py
+++ groundstation/objects/object_factory.py
@@ -1,8 +1,29 @@
+from groundstation.objects.base_object_pb2 import BaseObject, \
+    ROOT as TYPE_ROOT, \
+    UPDATE as TYPE_UPDATE, \
+    UNSET as TYPE_UNSET
 from groundstation.objects.root_object import RootObject
 from groundstation.objects.update_object import UpdateObject
 
+
+def type_of(protobuf):
+    base = BaseObject()
+    base.ParseFromString(protobuf)
+    return base.type
+
+
 def hydrate_object(protobuf):
-    obj = RootObject.from_object(protobuf)
-    if not obj.protocol:
-        obj = UpdateObject.from_object(protobuf)
-    return obj
+    # Test if it's strongly typed first
+    _type = type_of(protobuf)
+    if _type == TYPE_ROOT:
+        return RootObject.from_object(protobuf)
+    elif _type == TYPE_UPDATE:
+        return UpdateObject.from_object(protobuf)
+    elif _type == TYPE_UNSET:
+        # Use existing heuristic
+        obj = RootObject.from_object(protobuf)
+        if not obj.protocol:
+            obj = UpdateObject.from_object(protobuf)
+        return obj
+    else:
+        raise Exception("Unknown type; %s" % (str(_type)))
--- test/test_object_discrimination.py
+++ test/test_object_discrimination.py
@@ -1,5 +1,7 @@
 import unittest
 
+import groundstation.objects.object_factory as object_factory
+
 from groundstation.objects.base_object_pb2 import BaseObject, \
     ROOT as TYPE_ROOT, \
     UPDATE as TYPE_UPDATE, \
@@ -25,6 +27,24 @@ def new_update_object(weak, parents=[]):
     if not weak:
         update.type = TYPE_UPDATE
     return update
+
+
+class TypeOfTestCase(unittest.TestCase):
+    def test_strongly_typed_root(self):
+        root_str = new_root_object(False).SerializeToString()
+        self.assertEqual(object_factory.type_of(root_str), TYPE_ROOT)
+
+    def test_weakly_typed_root(self):
+        root_str = new_root_object(True).SerializeToString()
+        self.assertEqual(object_factory.type_of(root_str), TYPE_UNSET)
+
+    def test_strongly_typed_update(self):
+        update_str = new_update_object(False).SerializeToString()
+        self.assertEqual(object_factory.type_of(update_str), TYPE_UPDATE)
+
+    def test_weakly_typed_update(self):
+        update_str = new_update_object(True).SerializeToString()
+        self.assertEqual(object_factory.type_of(update_str), TYPE_UNSET)
 
 
 class WeaklyTypedObjectDiscriminationTestCase(unittest.TestCase):
