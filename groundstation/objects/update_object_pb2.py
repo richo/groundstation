0d71c495fd89fe5dae44e0ef3e1c5a8b6e9d51cb
Richo Healey <richo@psych0tik.net>
2013-05-30 14:33:47 +1000

    Sanely set defaults on subobjects

--- groundstation/objects/update_object_pb2.py
+++ groundstation/objects/update_object_pb2.py
@@ -14,7 +14,7 @@ import groundstation.objects.base_object_pb2
 DESCRIPTOR = _descriptor.FileDescriptor(
   name='groundstation/objects/update_object.proto',
   package='',
-  serialized_pb='\n)groundstation/objects/update_object.proto\x1a\'groundstation/objects/base_object.proto\"N\n\x0cUpdateObject\x12\x0f\n\x07parents\x18\x01 \x03(\x0c\x12\x0c\n\x04\x64\x61ta\x18\x02 \x02(\x0c\x12\x1f\n\x04type\x18\x0f \x01(\x0e\x32\n.GizmoType:\x05UNSET')
+  serialized_pb='\n)groundstation/objects/update_object.proto\x1a\'groundstation/objects/base_object.proto\"O\n\x0cUpdateObject\x12\x0f\n\x07parents\x18\x01 \x03(\x0c\x12\x0c\n\x04\x64\x61ta\x18\x02 \x02(\x0c\x12 \n\x04type\x18\x0f \x01(\x0e\x32\n.GizmoType:\x06UPDATE')
 
 
 
@@ -43,7 +43,7 @@ _UPDATEOBJECT = _descriptor.Descriptor(
     _descriptor.FieldDescriptor(
       name='type', full_name='UpdateObject.type', index=2,
       number=15, type=14, cpp_type=8, label=1,
-      has_default_value=True, default_value=0,
+      has_default_value=True, default_value=2,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
@@ -57,7 +57,7 @@ _UPDATEOBJECT = _descriptor.Descriptor(
   is_extendable=False,
   extension_ranges=[],
   serialized_start=86,
-  serialized_end=164,
+  serialized_end=165,
 )
 
 _UPDATEOBJECT.fields_by_name['type'].enum_type = groundstation.objects.base_object_pb2._GIZMOTYPE
