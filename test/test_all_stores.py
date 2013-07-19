1b75fe13249eba8d951735ad422dc512b1e28caf
Richo Healey <richo@psych0tik.net>
2013-07-18 20:16:08 -0700

    Add testcase for database initialization

--- test/test_all_stores.py
+++ test/test_all_stores.py
@@ -1,6 +1,14 @@
+import os
+
 import store_fixture
 import groundstation.store
 
 
 class TestGitStore(store_fixture.StoreTestCase):
     storeClass = groundstation.store.git_store.GitStore
+
+    def test_creates_required_dirs(self):
+        for d in groundstation.store.git_store.GitStore.required_dirs:
+            path = os.path.join(self.path, d)
+            self.assertTrue(os.path.exists(path))
+            self.assertTrue(os.path.isdir(path))
