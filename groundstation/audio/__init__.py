84678737a198f98cdf9778fa943976177346542f
Richo Healey <richo@psych0tik.net>
2014-01-30 17:45:20 -0800

    setup an audio stream in the announcer

---
+++ groundstation/audio/__init__.py
@@ -0,0 +1,21 @@
+import pyaudio
+
+# Shamelessly stolen directly from quietnet
+RATE = 44100
+CHANNELS = 1
+FRAME_LENGTH = 3
+CHUNK = 256
+DATASIZE = CHUNK * FRAME_LENGTH
+SIGIL = "00"
+BEACON_TIMEOUT = 1
+
+FORMAT = pyaudio.paInt16
+
+p = pyaudio.PyAudio()
+
+def get_stream(a, **kwargs):
+    """a: a pyaudio.PyAudio"""
+    stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, output=True)
+    default_args = dict(format=FORMAT, channels=CHANNELS, rate=RATE)
+    default_args.update(kwargs)
+    return a.open(**default_args)
--- groundstation/audio_announcer.py
+++ groundstation/audio_announcer.py
@@ -1,9 +1,16 @@
+import audio
+import quietnet
 import pyaudio
+
+from groundstation import logger
+log = logger.getLogger(__name__)
+
 
 class AudioAnnouncer(object):
     def __init__(self, freq):
         self.audio = pyaudio.PyAudio()
         self.freq = freq
+        self.stream = audio.get_stream(self.audio, output=True)
 
     def ping(self):
-        return None
+        log.info("Broadcasting audio ping")
